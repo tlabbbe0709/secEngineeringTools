6e57f3a7d19edc14f2336b6a0fa0369af9013882
tlabbe-sp6 <tlabbe@sp6.io>
2026-02-13 10:55:40 -0500

    u

--- Splunk/scripts/splunkInstall.sh
+++ Splunk/scripts/splunkInstall.sh
@@ -3,19 +3,19 @@ set -e
 
 if [[$(pwd) == /opt ]]; then
 	echo "Downloading install file"
-	wget -O splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz "https://download.splunk.com/products/splunk/releases/9.4.1/linux/splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz"
+	wget -O splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz "wget -O splunk-10.2.0-d749cb17ea65-linux-amd64.tgz "https://download.splunk.com/products/splunk/releases/10.2.0/linux/splunk-10.2.0-d749cb17ea65-linux-amd64.tgz""
 else
 	echo "Changing directories to /opt"
 	cd /opt
 	echo "Downloading install file"
-	wget -O splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz "https://download.splunk.com/products/splunk/releases/9.4.1/linux/splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz"
+	wget -O splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz "wget -O splunk-10.2.0-d749cb17ea65-linux-amd64.tgz "https://download.splunk.com/products/splunk/releases/10.2.0/linux/splunk-10.2.0-d749cb17ea65-linux-amd64.tgz""
 fi
 
 echo "Adding splunk user"
 adduser splunk
 
 echo "Untaring install file"
-tar -xvzf splunk-9.4.1-e3bdab203ac8-linux-amd64.tgz
+tar -xvzf splunk-10.2.0-d749cb17ea65-linux-amd64.tgz
 
 echo "ensuring ownership of splunk file to splunk user"
 chown -R splunk: /opt/splunk
